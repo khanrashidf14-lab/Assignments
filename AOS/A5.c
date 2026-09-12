47c234232829053355168dc56b89350991eed42c
khanrashidf14-lab <khanrashidf14@gmail.com>
2026-09-12 19:36:47 +0530

    Update and rename 5.c to A5.c

--- AOS/A5.c
+++ AOS/A5.c
@@ -72,3 +72,69 @@ int main() {
 
     return 0;
 }
+
+
+2.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void sigcont_handler(int sig) {
+    printf("Child: Received SIGCONT – resuming work\n");
+    fflush(stdout);
+}
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        exit(1);
+    }
+
+    if (pid == 0) {  /* Child process */
+        /* Catch SIGCONT so we can print a message when resumed.
+           Note: SIGSTOP cannot be caught or ignored. */
+        signal(SIGCONT, sigcont_handler);
+
+        printf("Child (PID %d) started. Working...\n", getpid());
+        fflush(stdout);
+
+        int counter = 0;
+        while (1) {
+            printf("Child: working... count = %d\n", ++counter);
+            fflush(stdout);
+            sleep(1);
+        }
+    } else {  /* Parent process */
+        printf("Parent (PID %d): Child PID = %d\n", getpid(), pid);
+        fflush(stdout);
+
+        sleep(3);   /* Let the child run for a few seconds */
+
+        printf("Parent: Sending SIGSTOP – suspending child\n");
+        fflush(stdout);
+        kill(pid, SIGSTOP);
+
+        sleep(5);   /* Child is suspended for 5 seconds */
+
+        printf("Parent: Sending SIGCONT – resuming child\n");
+        fflush(stdout);
+        kill(pid, SIGCONT);
+
+        sleep(4);   /* Let the child run a bit more */
+
+        printf("Parent: Sending SIGTERM – terminating child\n");
+        fflush(stdout);
+        kill(pid, SIGTERM);
+
+        wait(NULL);
+        printf("Parent: Child terminated. Exiting.\n");
+    }
+
+    return 0;
+}
